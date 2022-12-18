ac82df34f910bf4fa627e7010e9bdcd054a9fd2b
Natthapee Sriarunluck <natthapeesri@gmail.com>
2022-12-18 15:38:09 +0700

    initial commit

--- m2-starter/src/main/java/MinOps.java
+++ m2-starter/src/main/java/MinOps.java
@@ -2,63 +2,15 @@ import java.util.*;
 import java.util.List;
 
 public class MinOps {
-    static class UndirectedAdjMap<Vertex> {
-        Map<Vertex, Set<Vertex>> graph;
-        int edges;
 
-        public UndirectedAdjMap() {
-            this.graph = new HashMap<>();
-            this.edges = 0;
-        }
-
-        public Iterable<Vertex> adj(Vertex v) {
-            return graph.get(v);
-        }
-
-
-        public void addVertex(Vertex v) {
-            graph.put(v, new HashSet<>());
-        }
-
-        public void addEdge(Vertex u, Vertex v) {
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-            edges++;
-        }
-
-        public void removeEdge(Vertex u, Vertex v) {
-            graph.remove(u);
-            graph.remove(v);
-        }
-    }
-
-    UndirectedAdjMap<ArrayList<Integer>> graph = new UndirectedAdjMap<>();
-
-    public int minimumOps(List<Integer> S, List<Integer> T) {
-        ArrayList<Integer> s = new ArrayList<>(S);
-        ArrayList<Integer> t = new ArrayList<>(T);
-        HashSet<ArrayList<Integer>> allSequence = new HashSet<>();
-        HashSet<ArrayList<Integer>> currentLayer = new HashSet<>();
-        currentLayer.add(s);
-        for (ArrayList<Integer>current : currentLayer) {
-            graph.addVertex(current);
-
-            ArrayList<Integer> rotate = rotate(current);
-            ArrayList<Integer> reverse = reverse(current);
-
-            graph.addVertex(rotate);  allSequence.add(rotate); graph.addEdge(current,rotate);
-            graph.addVertex(reverse); allSequence.add(reverse); graph.addEdge(current, reverse);
-            if (allSequence.contains(t)){ break;}
-            current.clear();
-            currentLayer.add(rotate); currentLayer.add(reverse);
-        }
+    public static int minimumOps(List<Integer> S, List<Integer> T) {
         int count = 0;
-        Set<ArrayList<Integer>> frontier = new HashSet<>(List.of(s));
-        Set<ArrayList<Integer>> visited = new HashSet<>(List.of(s));
+        Set<List<Integer>> frontier = new HashSet<>(List.of(S));
+        Set<List<Integer>> visited = new HashSet<>(List.of(S));
 
         while (!frontier.isEmpty()) {
-            if (frontier.contains(t)) return count;
-            frontier = nbrs(graph, frontier);
+            if (frontier.contains(T)) return count;
+            frontier = nbrs(frontier);
             count++;
             frontier.removeAll(visited);
             visited.addAll(frontier);
@@ -66,22 +18,18 @@ public class MinOps {
         return -1;
     }
 
-    Set<ArrayList<Integer>> nbrs(UndirectedAdjMap<ArrayList<Integer>> G, Set<ArrayList<Integer>> F) {
-        Set<ArrayList<Integer>> nbrSet = new HashSet<>();
-        for (ArrayList<Integer> src : F) {
-            // function to find adj vertex to the source
-            for (ArrayList<Integer> dst : G.adj(src)) {
-                nbrSet.add(dst);
-            }
+    public static Set<List<Integer>> nbrs(Set<List<Integer>> F) {
+        Set<List<Integer>> nbrSet = new HashSet<>();
+        for (List<Integer> src : F) {
+            nbrSet.add(rotate(src));
+            nbrSet.add(reverse(src));
         }
         return nbrSet;
     }
 
 
-    public static ArrayList<Integer> rotate(ArrayList<Integer> lst) {
+    public static List<Integer> rotate(List<Integer> lst) {
         ArrayList<Integer> nlst = new ArrayList<>(lst);
-        System.out.println(nlst.size());
-        System.out.println(nlst);
         int lastIndex = lst.size()-1;
         int lastNumber = lst.get(lastIndex);
         nlst.remove(lastIndex);
@@ -89,7 +37,7 @@ public class MinOps {
         return nlst;
     }
 
-    public static ArrayList<Integer> reverse(ArrayList<Integer> lst)
+    public static List<Integer> reverse(List<Integer> lst)
     {
         ArrayList<Integer> revArrayList = new ArrayList<Integer>();
         for (int i = lst.size() - 1; i >= 0; i--) {
